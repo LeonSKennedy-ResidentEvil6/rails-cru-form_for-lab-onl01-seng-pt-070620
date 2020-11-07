efa0ffeb63008764fd048e524bd1b21dea8dac4d
LeonSKennedy-ResidentEvil6 <yiboli.coding@gmail.com>
2020-11-06 21:08:18 -0800

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,19 @@
 class ArtistsController < ApplicationController
+
+    def index
+        @artists = Artist.all
+    end 
+
+    def show
+        @artist = Artist.find(params[:id])
+    end
+
+    def new
+        @artist = Artist.new  
+    end 
+
+    def create
+        Artist.create(params.require(:artist).permit(:name, :bio))
+        redirect_to artists_path(@artist)
+    end 
 end
